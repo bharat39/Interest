b44e70e5dcf0584de0289e0ab5b7a190ec58b7a1
bharat39 <rogueeanne3@gmail.com>
2020-02-20 19:03:46 +0530

    Simple & Compound Interest

---
+++ Interest/src/main/java/maven/task4/CompoundInterest.java
@@ -0,0 +1,8 @@
+package maven.task4;
+
+public class CompoundInterest {
+	double compoundInterestCalculator(float principle,float time,float rateOfInterest)
+	{
+		return (principle*Math.pow(1+(rateOfInterest/100),time));
+	}
+}
